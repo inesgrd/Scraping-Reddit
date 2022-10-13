4336ab6e3cce685b516a0c6117713d6a48dc4219
inesgrd <ines.girard@sciencespo.fr>
2022-10-13 10:28:14 +0200

    Scrap reddit beautifulsoup 4

--- reddit recup.py
+++ reddit recup.py
@@ -1,11 +1,16 @@
+from inspect import Attribute
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
 import time
 from pprint import pprint
+from minet.scrape.std import get_display_text
 import json
 
-#Fonction qui récupère les posts dans une communauté reddit ; exemple avec r/space 
+## Script that scrap from reddit the 7th first posts in one community the title, link, date, votes, comments, and texts; user not available
+
+#choose the community; has to be written as it would be in the url
+#stored in a json file
 
 community = input("Choose a community on Reddit to scrap: ")
 
@@ -29,20 +34,130 @@ for post in allposts:
   nbcom = comments.split()
   nbcom.pop()
   nbcom = ''.join(nbcom)
-  result = {
-    "title": title,
-    "link": link,
-    "date": date,
-    "votes": votes,
-    "comments": comments
-  }
-  posts.append(result)
+  text = post.select_one(".Chtkt3BCZQruf0LtmFg2c")
+  if text is None:
+    result1 = {
+      "title": title,
+      "link": link,
+      "date": date,
+      "votes": votes,
+      "comments": comments
+    }
+    posts.append(result1)
+  else:
+    for i in text:
+      para = i.select("._1qeIAgB0cPwnLhDF9XSiJM")
+      para = get_display_text(para)
+      result2 = {
+        "title": title,
+        "link": link,
+        "date": date,
+        "text": para,
+        "votes": votes,
+        "comments": comments
+      }
+      posts.append(result2)
 tf = open("redditscrap_" + community + ".json", "w")
 json.dump(posts, tf)
 tf.close()
 
 
 ## Difficulties and questions ##
+
+## Questions: 
+#Vote : no other class to select, why? => json version to better get it
+#Why not possible to have more than 7 at a time ? => json version to do that
+#user not in the html, where to get it? => json version to do that
+
+
+## TEST 6: to get the text attached to the post ##
+# last working code but text not associted with the rest of the info from the psot
+
+#for i in text:
+#        if i is None:
+#            print("Nothing")
+#        else:
+#            para = i.select("._1qeIAgB0cPwnLhDF9XSiJM")
+#            para = get_display_text(para)
+#            result = {
+#              "text": para
+#            }
+#            posts.append(result)
+#    result = {
+#        "title": title,
+#        "link": link,
+#        "date": date,
+#        "votes": votes,
+#        "comments": comments
+#    }
+#    posts.append(result)
+#tf = open("redditscrap_" + community + ".json", "w")
+#json.dump(posts, tf)
+#tf.close()
+
+
+## TEST 5 : to get the text ## 
+#do not print anything
+
+#for post in allposts:
+#    title = post.select_one('a[data-click-id="body"]').get_text()
+#    link = post.select_one('a[data-click-id="body"]').get('href')
+#    date = post.select_one('span[data-testid="post_timestamp"]').get_text()
+#    votes = post.select_one("._1rZYMD_4xY3gRcSS3p8ODO").get_text()
+#    comments = post.select_one('a[data-click-id="comments"]').get_text()
+#    nbcom = comments.split()
+#    nbcom.pop()
+#    nbcom = ''.join(nbcom)
+#    text = post.select(".Chtkt3BCZQruf0LtmFg2c")
+#    for i in text:
+#        if i is None:
+#          result1 = {
+#            "title": title,
+#            "link": link,
+#            "date": date,
+#            "votes": votes,
+#            "comments": comments
+#          }
+#          posts.append(result1)
+#          pprint(posts)
+#        else:
+#            para = i.select("._1qeIAgB0cPwnLhDF9XSiJM")
+#            para = get_display_text(para)
+#            result2 = {
+#              "title": title,
+#              "link": link,
+#              "date": date,
+#              "text": para,
+#              "votes": votes,
+#              "comments": comments
+#            }
+#            posts.append(result2)
+#            pprint(posts)
+#pprint(posts)
+
+## TEST 4 : trying to get text and the rest together ##
+
+#text appears in another part in the json document ; it is next to the right information but not in the same pack
+#if the text is put together with the rest, then when there is no text, no information is collected
+#either have the text separated and have all or only have post with text
+#for i in text:
+#        if i is None:
+#            print("Nothing")
+#        else:
+#            para = i.select("._1qeIAgB0cPwnLhDF9XSiJM")
+#            para = get_display_text(para)
+#            result = {
+#              "title": title,
+#              "link": link,
+#              "text": para,
+#              "date": date,
+#              "votes": votes,
+#              "comments": comments
+#            }
+#            posts.append(result)
+
+
+## TEST 3 : trying to get user (nope) ##
 
 ##User : not in the html page !!!!!! 
 #Not working in python for user 
@@ -52,9 +167,6 @@ tf.close()
 #USER not in .Post why??? ; not possible 
 #=> Ask Benjamin where to get it 
 
-#Vote : no other class to select
-
-#Why not possible to have more than 7 at a time ?
 
 ## TEST 2 : Working with other class that should not change ##
 
